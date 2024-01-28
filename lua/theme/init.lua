95906d356e2071103d4e414cea3700b1adc41bd6
5-pebbles <5-pebble@protonmail.com>
2024-01-27 21:29:42 -0500

    fix: indent-blankline scope options

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -1,7 +1,17 @@
 local M = {
     -- Indent lines
     {
-        { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+        {
+            "lukas-reineke/indent-blankline.nvim",
+            main = "ibl",
+            opts = {
+                scope = {
+                    enabled = true,
+                    show_start = false,
+                    show_end = false,
+                },
+            }
+        }
     },
     require("theme.onedark")
 }
