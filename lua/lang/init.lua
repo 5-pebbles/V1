0721938b9438967e105d275ef374cbff36f5c9c9
5-pebbles <5-pebble@protonmail.com>
2024-01-30 18:59:22 -0500

    feat(autocomplete): add search & cmd path autocomplete.

--- lua/lang/init.lua
+++ lua/lang/init.lua
@@ -36,12 +36,39 @@ local M = {
     },
     -- AutoCompletion
     {
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    {
         "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-buffer",
+        },
         config = function()
             local cmp = require("cmp")
+
+            -- `/` cmdline setup.
+            cmp.setup.cmdline('/', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
+            -- `:` cmdline setup.
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' }
+                        }
+                    }
+                })
+            })
+
             cmp.setup({
                 enabled = function()
                     return true
@@ -109,8 +136,8 @@ local M = {
                         end,
                     },
                     { name = "luasnip" },
-                }, {
-                    { name = "buffer" },
+                    { name = 'buffer', keyword_length = 3 },
+                    { name = 'path' },
                 }),
             })
         end,
