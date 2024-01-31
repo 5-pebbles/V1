9ac269cf21dcd20c09d7af78e0666b23d6ed422c
5-pebbles <5-pebble@protonmail.com>
2024-01-31 09:35:19 -0500

    rm(plugins.telescope)!: remove telescope keybindings moved to cmdline in 96d8e9b5cc082a1e690a19410d36bdc3e14c02ba BREAKING CHANGE: telescope keybindings removed.

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -2,95 +2,9 @@ local M = {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
     config = function()
-        local action_state = require("telescope.actions.state")
-        local actions = require("telescope.actions")
-
-        -- Delete
-        local function delete_file(prompt_bufnr)
-            local selection = action_state.get_selected_entry(prompt_bufnr)
-
-            actions.close(prompt_bufnr)
-
-            local answer = vim.fn.input("Remove " .. selection.value .. " y/N: ")
-
-            local lower_answer = string.lower(answer)
-
-            if lower_answer == "yes" or lower_answer == "y" then
-                local scandir = function(directory)
-                    local i, t, popen = 0, {}, io.popen
-                    local pfile = popen('ls -a "' .. directory .. '"')
-                    if pfile == nil then
-                        return {}
-                    end
-                    for filename in pfile:lines() do
-                        i = i + 1
-                        t[i] = filename
-                    end
-                    pfile:close()
-                    return t
-                end
-
-                local function walk(directory)
-                    directory = directory:match("(.*)/[^/]+$")
-                    for _, i in ipairs(scandir(directory)) do
-                        if i ~= "." and i ~= ".." then
-                            return
-                        end
-                    end
-                    os.remove(directory)
-                    walk(directory)
-                end
-
-                -- Delete the file
-                os.remove(selection.value)
-
-                -- Get the directory of the file
-                walk(selection.value)
-            end
-
-
-            vim.api.nvim_command("Telescope resume")
-        end
-
-        -- New Buffer
-        local function create_file(prompt_bufnr)
-            local current_picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-            local prompt = current_picker:_get_prompt()
-
-            actions.close(prompt_bufnr)
-
-            local answer = vim.fn.input("Create File: ", prompt)
-
-            local current_dir = vim.fn.expand("%:p:h")
-            if answer:sub(1, 2) == "./" then
-                answer = current_dir .. "/" .. answer:sub(3)
-            end
-            vim.api.nvim_command("e " .. answer)
-        end
-
         require("telescope").setup({
             defaults = {
                 file_ignore_patterns = { "%.git/.*" },
-                mappings = {
-                    i = {
-                        ["<C-d>"] = delete_file,
-                        ["<C-n>"] = create_file,
-                    },
-                    n = {
-                        ["<C-d>"] = delete_file,
-                        ["<C-n>"] = create_file,
-                        ["/"] = function(prompt_bufnr)
-                            local selection = require("telescope.actions.state").get_selected_entry()
-                            local dir = vim.fn.fnamemodify(selection.path, ":p:h")
-                            require("telescope.actions").close(prompt_bufnr)
-                            vim.cmd(string.format("silent lcd %s", dir))
-                        end,
-                        ["."] = function(prompt_bufnr)
-                            require("telescope.actions").close(prompt_bufnr)
-                            vim.cmd("silent lcd ..")
-                        end,
-                    },
-                },
                 layout_config = {
                     prompt_position = "top",
                 },
